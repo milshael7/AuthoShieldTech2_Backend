5c37e59b7e5c0b4f56298ef9333700c1bf1e265d
milshael7 <wdelvoix23@gmail.com>
2026-01-30 21:08:13 -0600

    Update jwt.js

--- backend/src/auth/jwt.js
+++ backend/src/auth/jwt.js
@@ -1,4 +1,9 @@
 const jwt = require('jsonwebtoken');
-const sign = (payload, secret, expiresIn='7d') => jwt.sign(payload, secret, { expiresIn });
-const verify = (token, secret) => jwt.verify(token, secret);
+
+const sign = (payload, secret, expiresIn = '7d') =>
+  jwt.sign(payload, secret, { expiresIn });
+
+const verify = (token, secret) =>
+  jwt.verify(token, secret);
+
 module.exports = { sign, verify };
